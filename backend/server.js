48065f56a21245e54b0a6086e342e95b0fc2e8e5
Emanuel Tomasello <emanueltomasello81@gmail.com>
2026-09-04 18:16:45 +0200

    Aggiunte cartelle per server e database

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const express = require('express');
+const app = express(); // instanziamo un oggetto express, che rappresenta la nostra applicazione
+
+// in questo modo possiamo definire le rotte, gestire le richieste HTTP, configurazioni
+// Porta del server
+const PORT = 3000;
+
+// Permette al server di leggere dati in formato JSON
+app.use(express.json());
+
+// Rotta base
+app.get('/', (req, res) => {
+res.send('Server attivo');
+});
+// Avvia il server
+
+app.listen(PORT, () => {
+console.log(`Server in ascolto su http://localhost:${PORT}`);
+});
